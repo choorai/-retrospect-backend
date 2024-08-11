5f696961a8c12e46ec6371989204dc3299ebb6a6
JoungMinJu <alswn9938@gmail.com>
2024-08-11 19:55:16 +0900

    feat : User entity 생성

---
+++ src/main/java/choorai/retrospect/user/entity/User.java
@@ -0,0 +1,45 @@
+package choorai.retrospect.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(indexes = {
+    @Index(name = "idx_user_id", columnList = "user_id", unique = true)
+})
+@Entity
+public class User {
+
+    @Id
+    @Column(name = "user_id", length = 320, nullable = false)
+    private String userId;
+
+    @Column(name = "password", length = 255, nullable = false)
+    private String password;
+
+    @Column(name = "name", length = 5, nullable = false)
+    private String name;
+
+    @Column(name = "department", length = 50, nullable = false)
+    private String department;
+
+    @Column(name = "position", length = 50, nullable = false)
+    private String position;
+
+    @Column(name = "company_name", length = 50, nullable = false)
+    private String companyName;
+
+    public User(String userId, String password, String name, String department, String position, String companyName) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.companyName = companyName;
+    }
+}
