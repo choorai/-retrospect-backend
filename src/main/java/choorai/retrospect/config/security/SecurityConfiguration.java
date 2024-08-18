808acaa674c8a15d2d5280e1a805fbec197b5a4f
seokhwan-an <asok369258@naver.com>
2024-08-18 11:40:08 +0900

    RH-3 : cors 설정 추가

--- src/main/java/choorai/retrospect/config/security/SecurityConfiguration.java
+++ src/main/java/choorai/retrospect/config/security/SecurityConfiguration.java
@@ -3,9 +3,13 @@ package choorai.retrospect.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -14,12 +18,30 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain healthCheckFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
             .securityMatcher(antMatcher("/actuator/**"))
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers(antMatcher("/actuator/health")).permitAll();
                 auth.anyRequest().authenticated();
+            })
+            .build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain corsFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .cors(corsConfig -> {
+                corsConfig.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedMethods(List.of("POST", "GET", "PUT", "PATCH", "DELETE"));
+                    // TODO : 배포후 연동 시 변경
+                    configuration.addAllowedOrigin("*");
+                    configuration.addAllowedHeader("*");
+                    return configuration;
+                });
             })
             .build();
     }
