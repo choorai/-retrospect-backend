49a34794622337b37486f99ee30a58f1a915e22f
JoungMinJu <alswn9938@gmail.com>
2024-08-24 22:44:45 +0900

    refactor : flyway .sql 삭제

--- src/main/resources/db/migration/V240811_1932_create_user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user (
-    user_id VARCHAR(320) NOT NULL PRIMARY KEY,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(5) NOT NULL,
-    department VARCHAR(50) NOT NULL,
-    position VARCHAR(50) NOT NULL,
-    company_name VARCHAR(50) NOT NULL
-);
