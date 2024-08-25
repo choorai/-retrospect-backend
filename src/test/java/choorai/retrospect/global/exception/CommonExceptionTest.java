ea9a956ad3ffd63301d185384f5245f1cdf94bca
JoungMinJu <alswn9938@gmail.com>
2024-08-25 22:07:18 +0900

    refactor : Exception 테스트

--- src/test/java/choorai/retrospect/global/exception/CommonExceptionTest.java
+++ src/test/java/choorai/retrospect/global/exception/CommonExceptionTest.java
@@ -1,7 +1,12 @@
 package choorai.retrospect.global.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +21,19 @@ class CommonExceptionTest {
                                                                     Map.of("정보1", "내용1", "정보2", "내용2"));
         // when
         final String inputValue = commonException.getInputValue();
+
         // then
-        assertThat(inputValue)
-            .isEqualTo("{정보1 : 내용1, 정보2 : 내용2}");
+        final String prefix = inputValue.substring(0, 1);
+        final String rawValue = inputValue.substring(1, inputValue.length() - 1);
+        final List<String> values = Arrays.asList(rawValue.split(", "));
+        final String suffix = inputValue.substring(inputValue.length() - 1);
+        assertAll(
+                  () -> assertEquals("{", prefix),
+                  () -> assertEquals("}", suffix),
+                  () -> assertEquals(2, values.size()),
+                  () -> assertTrue(values.contains("정보1 : 내용1")),
+                  () -> assertTrue(values.contains("정보2 : 내용2"))
+        );
     }
 
 }
