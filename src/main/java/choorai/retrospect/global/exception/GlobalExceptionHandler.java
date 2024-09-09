4f6a7df0f0211791ab52aed2c4e35d30760bffdc
JoungMinJu <alswn9938@gmail.com>
2024-09-09 19:36:41 +0900

    RH-14 : Exception 반환 dto 수정

--- src/main/java/choorai/retrospect/global/exception/GlobalExceptionHandler.java
+++ src/main/java/choorai/retrospect/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package choorai.retrospect.global.exception;
 
 import choorai.retrospect.global.exception.dto.CustomExceptionResponse;
+import choorai.retrospect.global.response.BaseResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,12 +15,14 @@ public class GlobalExceptionHandler {
     private static final String ERROR_LOG_FORMAT_WITH_WRONG_INPUT = "url={}, errorCode={}, errorMessage={}, errorInput={}";
 
     @ExceptionHandler(CommonException.class)
-    public CustomExceptionResponse handleException(
+    public BaseResponseDto<CustomExceptionResponse> handleException(
         final CommonException exception,
         final HttpServletRequest request
     ) {
         trackLog(exception, request);
-        return new CustomExceptionResponse(exception.getCode(), exception.getMessage());
+        final CustomExceptionResponse customExceptionResponse = new CustomExceptionResponse(exception.getCode(),
+                                                                                            exception.getMessage());
+        return BaseResponseDto.ofException(customExceptionResponse);
     }
 
     private void trackLog(final CommonException exception, final HttpServletRequest request) {
