2b43b90db6764c344b9d5a4a2f9c3664e8f5ccb1
JoungMinJu <alswn9938@gmail.com>
2024-09-09 19:58:16 +0900

    RH-16 : AuthException 에러코드 재정의

--- src/main/java/choorai/retrospect/auth/exception/AuthErrorCode.java
+++ src/main/java/choorai/retrospect/auth/exception/AuthErrorCode.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 
 public enum AuthErrorCode implements ErrorCode {
 
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 입력", "아이디 또는 비밀번호가 잘못됐습니다."),
-    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 입력", "아이디 또는 비밀번호가 잘못됐습니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"잘못된 입력", "refresh token이 유효하지 않습니다."),
-    REFRESH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 입력", "refresh token이 존재하지 않습니다."),
-    REFRESH_TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "잘못된 입력", "refresh token이 만료되었습니다.");
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "2001", "아이디 또는 비밀번호가 잘못됐습니다."),
+    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "2002", "아이디 또는 비밀번호가 잘못됐습니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST,"2003", "refresh token이 유효하지 않습니다."),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "2004", "refresh token이 존재하지 않습니다."),
+    REFRESH_TOKEN_IS_EXPIRED(HttpStatus.BAD_REQUEST, "2005", "refresh token이 만료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String errorCode;
