d859565c3d2ed58a5865bfd45745360cb967f807
JoungMinJu <alswn9938@gmail.com>
2024-09-12 21:24:11 +0900

    RH-16 : RetrospectRoomErrorCode 재정의

--- src/main/java/choorai/retrospect/retrospect_room/excpetion/RetrospectRoomErrorCode.java
+++ src/main/java/choorai/retrospect/retrospect_room/excpetion/RetrospectRoomErrorCode.java
@@ -6,15 +6,15 @@ import org.springframework.http.HttpStatus;
 public enum RetrospectRoomErrorCode implements ErrorCode {
 
     // TODO : errorCode는 추후 회의를 통해 숫자로 변경
-    SUBJECT_LENGTH_ERROR(HttpStatus.BAD_REQUEST, "잘못된 입력", "회고 주제의 글자 길이는 200자를 넘길 수 없습니다."),
-    DETAILS_LENGTH_ERROR(HttpStatus.BAD_REQUEST, "잘못된 입력", "회고 상세 내용은 500자 미만이어야 합니다."),
-    SHARE_LINK_ERROR(HttpStatus.BAD_REQUEST, "잘못된 입력", "공유 링크 크기는 200자를 넘길 수 없습니다");
+    SUBJECT_LENGTH_ERROR(HttpStatus.BAD_REQUEST, 3001, "회고 주제의 글자 길이는 200자를 넘길 수 없습니다."),
+    DETAILS_LENGTH_ERROR(HttpStatus.BAD_REQUEST, 3002, "회고 상세 내용은 500자 미만이어야 합니다."),
+    SHARE_LINK_ERROR(HttpStatus.BAD_REQUEST, 3003, "공유 링크 크기는 200자를 넘길 수 없습니다");
 
     private final HttpStatus httpStatus;
-    private final String errorCode;
+    private final int errorCode;
     private final String errorMessage;
 
-    RetrospectRoomErrorCode(HttpStatus httpStatus, String errorCode, String errorMessage) {
+    RetrospectRoomErrorCode(HttpStatus httpStatus, int errorCode, String errorMessage) {
         this.httpStatus = httpStatus;
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
@@ -26,7 +26,7 @@ public enum RetrospectRoomErrorCode implements ErrorCode {
     }
 
     @Override
-    public String getCode() {
+    public int getCode() {
         return errorCode;
     }
 
