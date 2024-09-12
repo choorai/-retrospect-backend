6dafc367b458d508ef2b2b52cb9b56167afa6c3f
seokhwan-an <asok369258@naver.com>
2024-09-12 20:42:43 +0900

    RH-20 : TimeLimit 필드 객체로 변경 및 생성자의 접근 제어자 private로 수정

--- src/main/java/choorai/retrospect/retrospect_room/entity/RetrospectRoom.java
+++ src/main/java/choorai/retrospect/retrospect_room/entity/RetrospectRoom.java
@@ -1,16 +1,13 @@
 package choorai.retrospect.retrospect_room.entity;
 
 import choorai.retrospect.global.domain.BaseEntity;
-import choorai.retrospect.retrospect_room.entity.value.Details;
-import choorai.retrospect.retrospect_room.entity.value.ShareLink;
-import choorai.retrospect.retrospect_room.entity.value.Subject;
-import choorai.retrospect.retrospect_room.entity.value.Type;
+import choorai.retrospect.retrospect_room.entity.value.*;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Time;
-
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class RetrospectRoom extends BaseEntity {
@@ -28,8 +25,27 @@ public class RetrospectRoom extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private Type type;
 
-    private Time timeLimit;
+    @Embedded
+    private TimeLimit timeLimit;
 
     @Embedded
     private ShareLink shareLink;
+
+    private RetrospectRoom(final Long id, final Subject subject, final Details details, final Type type, final TimeLimit timeLimit, final ShareLink shareLink) {
+        this.id = id;
+        this.subject = subject;
+        this.details = details;
+        this.type = type;
+        this.timeLimit = timeLimit;
+        this.shareLink = shareLink;
+    }
+
+    public static RetrospectRoom forSave(final String subject, final String details, final String type, final String timeLimit, final String shareLink) {
+        return new RetrospectRoom(null,
+            new Subject(subject),
+            new Details(details),
+            Type.valueOf(type),
+            TimeLimit.from(timeLimit),
+            new ShareLink(shareLink));
+    }
 }
