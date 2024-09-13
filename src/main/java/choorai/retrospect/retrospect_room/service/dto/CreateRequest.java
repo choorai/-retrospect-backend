b6f9df9671f4d548bfb819490cd3fddd5ca17dc1
seokhwan-an <asok369258@naver.com>
2024-09-13 16:12:53 +0900

    RH-20 : 회고 룸 생성 기능 테스트 추가

--- src/main/java/choorai/retrospect/retrospect_room/service/dto/CreateRequest.java
+++ src/main/java/choorai/retrospect/retrospect_room/service/dto/CreateRequest.java
@@ -1,9 +1,11 @@
 package choorai.retrospect.retrospect_room.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class CreateRequest {
 
