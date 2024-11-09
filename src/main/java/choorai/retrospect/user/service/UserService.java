b949613918d83c4064ddcbcdd94cc30ea0b514c0
JoungMinJu <alswn9938@gmail.com>
2024-11-09 11:55:02 +0900

    RH-22 : User의 cards를 가져올 때 lazy Exception 발생 해결

--- src/main/java/choorai/retrospect/user/service/UserService.java
+++ src/main/java/choorai/retrospect/user/service/UserService.java
@@ -1,14 +1,31 @@
 package choorai.retrospect.user.service;
 
 import choorai.retrospect.user.entity.User;
+import choorai.retrospect.user.entity.repository.UserRepository;
+import choorai.retrospect.user.exception.UserErrorCode;
+import choorai.retrospect.user.exception.UserException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
 
     public User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return (User) authentication.getPrincipal();
+    }
+
+    public User getCurrentUserWithCards() {
+        final User currentUser = getCurrentUser();
+        final Long id = currentUser.getId();
+
+        return userRepository.findWithCardsById(id)
+            .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND_FOR_ID));
     }
 
 }
