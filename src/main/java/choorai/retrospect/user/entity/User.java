d0224309e55fa9e06c4e9d554a2d109484443740
JoungMinJu <alswn9938@gmail.com>
2024-11-10 22:11:07 +0900

    RH-22 : User 생성자에 Role 추가

--- src/main/java/choorai/retrospect/user/entity/User.java
+++ src/main/java/choorai/retrospect/user/entity/User.java
@@ -1,11 +1,13 @@
 package choorai.retrospect.user.entity;
 
 import choorai.retrospect.global.domain.BaseEntity;
+import choorai.retrospect.retrospect_room.card.entity.Card;
 import choorai.retrospect.user.entity.value.Email;
 import choorai.retrospect.user.entity.value.Name;
 import choorai.retrospect.user.entity.value.Password;
 import choorai.retrospect.user.entity.value.Role;
 import jakarta.persistence.*;
+import java.util.ArrayList;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -44,13 +46,14 @@ public class User extends BaseEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    public User(String email, String password, String name, String companyName, String department, String position) {
+    public User(String email, String password, String name, String companyName, String department, String position, Role role) {
         this.email = new Email(email);
         this.password = new Password(password);
         this.name = new Name(name);
         this.companyName = companyName;
         this.department = department;
         this.position = position;
+        this.role = role;
     }
 
     public User(String email, String password, String name) {
