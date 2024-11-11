502a1458a7071eb6eb1383ea55c11ddba8a85202
JoungMinJu <alswn9938@gmail.com>
2024-11-11 21:56:32 +0900

    RH-22 : 스키마 추가

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -9,3 +9,16 @@ CREATE TABLE if NOT EXISTS retrospect_room
     created_at datetime(6),
     updated_at datetime(6)
 );
+
+CREATE TABLE if NOT EXISTS card
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content    VARCHAR(255),
+    type       enum('KEEP','PROBLEM','TRY'),
+    retrospect_room_id BIGINT,
+    user_id BIGINT,
+    created_at datetime(6),
+    updated_at datetime(6),
+    CONSTRAINT fk_retrospect_room FOREIGN KEY (retrospect_room_id) REFERENCES retrospect_room(id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id)
+);
