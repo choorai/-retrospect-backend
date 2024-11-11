174b6088d305e50fca306ffc438fdfe5fbe245c7
JoungMinJu <alswn9938@gmail.com>
2024-11-11 21:47:40 +0900

    RH-22 : content 객체로 수정

--- src/main/java/choorai/retrospect/retrospect_room/card/entity/value/Content.java
+++ src/main/java/choorai/retrospect/retrospect_room/card/entity/value/Content.java
@@ -2,17 +2,26 @@ package choorai.retrospect.retrospect_room.card.entity.value;
 
 import choorai.retrospect.retrospect_room.card.exception.CardErrorCode;
 import choorai.retrospect.retrospect_room.card.exception.CardException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
 public class Content {
 
+    @Column(name = "content")
     private String value;
 
     public Content(final String value) {
-        validateContent(value);
+        validate(value);
         this.value = value;
     }
 
-    private void validateContent(final String content) {
+    private void validate(final String content) {
         if (content == null || content.isEmpty()) {
             throw new CardException(CardErrorCode.CONTENT_IS_NOT_NULL);
         }
