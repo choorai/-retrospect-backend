3f79eaa6565c44eb8d60196fdc1a8544c78381fe
JoungMinJu <alswn9938@gmail.com>
2024-11-16 00:27:17 +0900

    RH-23 : currentUser 가져오는 로직 추가

--- src/main/java/choorai/retrospect/user/entity/repository/UserRepository.java
+++ src/main/java/choorai/retrospect/user/entity/repository/UserRepository.java
@@ -3,10 +3,14 @@ package choorai.retrospect.user.entity.repository;
 import choorai.retrospect.user.entity.User;
 import choorai.retrospect.user.entity.value.Email;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(Email email);
+
+    @EntityGraph(attributePaths = {"cards"})
+    Optional<User> findWithCardsById(Long userId);
 
 }
