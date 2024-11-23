84df3e04b80401e58fe74d04e12a8d82d3af5846
JoungMinJu <alswn9938@gmail.com>
2024-11-23 21:14:27 +0900

    RH-23 : response, request 형식 통일

--- src/main/java/choorai/retrospect/retrospect_room/card/service/dto/CardCreateRequest.java
+++ src/main/java/choorai/retrospect/retrospect_room/card/service/dto/CardCreateRequest.java
@@ -2,14 +2,12 @@ package choorai.retrospect.retrospect_room.card.service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class CardCreateRequest {
 
-    private String type;
-    private String content;
+    private final String type;
+    private final String content;
 
 }
--- src/main/java/choorai/retrospect/retrospect_room/card/service/dto/CardResponse.java
+++ src/main/java/choorai/retrospect/retrospect_room/card/service/dto/CardResponse.java
@@ -1,20 +1,16 @@
 package choorai.retrospect.retrospect_room.card.service.dto;
 
 import choorai.retrospect.retrospect_room.card.entity.Card;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CardResponse {
 
     private final long id;
     private final String type;
     private final String content;
-
-    public CardResponse(final long id, final String type, final String content) {
-        this.id = id;
-        this.type = type;
-        this.content = content;
-    }
 
     public static CardResponse of(final Card card) {
         return new CardResponse(card.getId(), card.getType().toString(), card.getContent());
